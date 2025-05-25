1dbeb7e3e458efb63b07062286badc1020c55ebb
han-link <115926359+han-link@users.noreply.github.com>
2025-05-25 03:24:03 +0200

    fix countdown display showing NaN and incorrect 60:00 time

--- src/components/countdown-timer.tsx
+++ src/components/countdown-timer.tsx
@@ -1,19 +1,22 @@
 import { component$ } from "@builder.io/qwik";
 
 export const CountdownTimer = component$((props: { time: number }) => {
-  const dateObj = new Date(props.time * 1000);
-  const minutes = dateObj.getUTCMinutes();
-  const seconds = dateObj.getSeconds();
-  const timeString =
-    minutes.toString().padStart(2, "0") + seconds.toString().padStart(2, "0");
+  const minutes = Math.floor(props.time / 60);
+  const seconds = props.time % 60;
+  let minStr = "00";
+  let secStr = "00";
+  if (!isNaN(minutes) || !isNaN(seconds)) {
+    minStr = String(minutes).padStart(2, "0");
+    secStr = String(seconds).padStart(2, "0");
+  }
 
   return (
     <div class="flex max-w-sm text-center font-sans text-8xl text-white">
-      <div class="mr-1 rounded bg-gray-900 p-1">{timeString[0]}</div>
-      <div class="mx-1 rounded bg-gray-900 p-1">{timeString[1]}</div>
+      <div class="mr-1 rounded bg-gray-900 p-1">{minStr[0]}</div>
+      <div class="mx-1 rounded bg-gray-900 p-1">{minStr[1]}</div>
       <div class="text-gray-900">:</div>
-      <div class="mx-1 rounded bg-gray-900 p-1">{timeString[2]}</div>
-      <div class="ml-1 rounded bg-gray-900 p-1">{timeString[3]}</div>
+      <div class="mx-1 rounded bg-gray-900 p-1">{secStr[0]}</div>
+      <div class="ml-1 rounded bg-gray-900 p-1">{secStr[1]}</div>
     </div>
   );
 });
