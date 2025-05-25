090519e220f3226465e1b23e34d05d0689889203
han-link <115926359+han-link@users.noreply.github.com>
2025-05-25 03:29:10 +0200

    implement min/max limits on time input

--- src/components/time-selector.tsx
+++ src/components/time-selector.tsx
@@ -8,13 +8,19 @@ interface ITimeSelectProps {
 }
 
 export const TimeSelect = component$((props: ITimeSelectProps) => {
+  const min = 0;
+  const max = 60;
   return (
     <div class="relative w-full">
       <button
         id="decreaseButton"
         class="absolute right-9 bottom-1 cursor-pointer rounded-md border border-transparent p-1.5 text-center text-sm text-slate-600 transition-all hover:bg-slate-100 focus:bg-slate-100 active:bg-slate-100 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
         type="button"
-        onClick$={() => props.time.value--}
+        onClick$={() => {
+          if (props.time.value - 1 >= min) {
+            props.time.value--;
+          }
+        }}
         disabled={props.disabled}
         aria-label={"Increase " + props.accessibleName}
       >
@@ -36,18 +42,35 @@ export const TimeSelect = component$((props: ITimeSelectProps) => {
       <input
         id={"timeSelect-" + props.accessibleName.replace(" ", "")}
         type="number"
-        value={props.time.value}
-        onInput$={(event) =>
-          (props.time.value = Number((event.target as HTMLInputElement).value))
-        }
-        class="ease w-full appearance-none rounded-md border border-slate-200 bg-transparent py-2 pr-20 pl-3 text-sm text-slate-700 shadow-sm transition duration-300 placeholder:text-slate-400 hover:border-slate-300 focus:border-slate-400 focus:shadow focus:outline-none [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none"
+        bind:value={props.time}
+        onBlur$={(e) => {
+          const el = e.target as HTMLInputElement;
+          const value = Number(el.value);
+          if (value > max) {
+            el.value = String(max);
+            props.time.value = max;
+          } else if (value < min) {
+            el.value = String(min);
+            props.time.value = min;
+          } else if (el.value.trim() === "") {
+            el.value = String(min);
+            props.time.value = min;
+          }
+        }}
+        min={min}
+        max={max}
+        class="ease w-[16rem] appearance-none rounded-md border border-slate-200 bg-transparent py-2 pr-20 pl-3 text-sm text-slate-700 shadow-sm transition duration-300 placeholder:text-slate-400 hover:border-slate-300 focus:border-slate-400 focus:shadow focus:outline-none [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none"
         disabled={props.disabled}
       />
       <button
         id="increaseButton"
         class="absolute right-1 bottom-1 cursor-pointer rounded-md border border-transparent p-1.5 text-center text-sm text-slate-600 transition-all hover:bg-slate-100 focus:bg-slate-100 active:bg-slate-100 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
         type="button"
-        onClick$={() => props.time.value++}
+        onClick$={() => {
+          if (props.time.value + 1 <= max) {
+            props.time.value++;
+          }
+        }}
         disabled={props.disabled}
         aria-label={"Decrease " + props.accessibleName}
       >
